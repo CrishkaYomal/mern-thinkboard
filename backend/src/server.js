00973bc1aa1b250a3238c36069c4598b904caebd
CrishkaYomal <Crishkay2003@gmail.com>
2026-04-29 20:33:52 +0530

    prepare for the deployment

--- backend/src/server.js
+++ backend/src/server.js
@@ -5,6 +5,7 @@ import dotenv from 'dotenv';
 import ratelimit from './config/upstash.js';
 import ratelimiter from './middleware/ratelimiter.js';
 import cors from 'cors';
+import path from "path";
 
 dotenv.config();
 
@@ -12,12 +13,15 @@ dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT || 5001;
+const __dirname = path.resolve();
 
-
+if(process.env.NODE_ENV !== "production"){
 app.use(cors({
     origin: ['http://localhost:5173', 'http://localhost:5174', 'http://localhost:5175', 'http://localhost:5176'],
     credentials: true,
-}));
+})
+);
+}
 app.use(express.json()); // Middleware to parse JSON bodies
 app.use(ratelimiter); // Apply the rate limiter middleware to all routes
 
@@ -27,6 +31,14 @@ app.use(ratelimiter); // Apply the rate limiter middleware to all routes
 // });
 
 app.use("/api/notes", notesRoutes);
+
+if(process.env.NODE_ENV === "production"){
+    app.use(express.static(path.join(__dirname,"../frontend/vite-project/dist")));
+
+    app.get("*",(req,res) => {
+        res.sendFile(path.join(__dirname,"../frontend","dist","index.html"));
+    });
+};
 
 connectDB().then(() => {
     app.listen(PORT, () => {
